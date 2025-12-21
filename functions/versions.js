135e7e6fb34599358f5ec7729f3d6bb4e0bab969
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-20 20:01:15 -0500

    Add files via upload

--- functions/versions.js
+++ functions/versions.js
@@ -1,1 +1,12 @@
 
+export async function onRequest() {
+  return new Response(JSON.stringify({
+    full: "Available",
+    fAFP: "Available",
+    lite: "Available",
+    forms: "Available",
+    standard: "Available"
+  }, null, 2), {
+    headers: { "content-type": "application/json" }
+  });
+}
