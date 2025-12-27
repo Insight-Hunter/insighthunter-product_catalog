7cfe01094aa8dadcce51027483c25a2724219a7e
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-26 21:36:44 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   image: {
+    service: { entrypoint: 'astro/assets/services/sharp' },
+    domains: ['example.com'],
+    formats: ['avif', 'webp'],
+    quality: 80,
+  }
     remotePatterns: [{ protocol: 'https' }],
   },
 });
