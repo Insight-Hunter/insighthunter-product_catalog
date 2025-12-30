ae22ab61053567d6fbd146a6e25c1806faa8d627
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-29 23:32:57 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  image: {
-    service: { entrypoint: 'astro/assets/services/sharp' },
-    domains: ['example.com'],
-    formats: ['avif', 'webp'],
-    quality: 80,
-  }
-    remotePatterns: [{ protocol: 'https' }],
-  },
+  output: 'static', // or 'hybrid' if needed
+  adapter: cloudflare(),
 });
