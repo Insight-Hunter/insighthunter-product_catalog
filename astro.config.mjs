6956ed71e32ec19ff7d691dd9f12c5a28f23f24a
James Michael Hunter Turner <michael@turneratlanta.com>
2026-01-02 16:27:52 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  site: "https://astroship.web3templates.com",
-  integrations: [mdx(), sitemap(), icon()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [tailwind(), sitemap(), mdx(), icon()],
+  output: 'static',  // Explicitly set to static (often the default, but recommended here)
+  site: 'https://insighthunter.app'
 });
